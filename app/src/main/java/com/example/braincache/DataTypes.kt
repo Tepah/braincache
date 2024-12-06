7dc649c0750d2adbfdf03b533c5f87005d6be4ba
Tepah <67202848+Tepah@users.noreply.github.com>
2024-12-05 23:55:12 -0800

    Created the API services for retrofit

--- app/src/main/java/com/example/braincache/DataTypes.kt
+++ app/src/main/java/com/example/braincache/DataTypes.kt
@@ -2,13 +2,16 @@ package com.example.braincache
 
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 data class User (
     val id: String? = null,
     val username: String,
+    val password: String
 )
 
 data class Quiz(
@@ -18,5 +21,25 @@ data class Quiz(
     val answers: List<String>,
 )
 
-interface UserService {
+interface ApiService {
+    @POST("/users")
+    fun createUser(@Body user: User): Call<User>
+
+    @POST("/login")
+    fun loginUser(@Body user: User): Call<User>
+
+    @GET("/users/{userid}")
+    fun getUser(@Path("id") id: String): Call<List<User>>
+
+    @GET("/quizzes")
+    fun getAllQuizzes(): Call<List<Quiz>>
+
+    @GET("/quizzes/{quizid}")
+    fun getQuizzesByUser(@Path("userID") userID: String): Call<List<Quiz>>
+
+    @POST("quizzes")
+    fun createQuiz(@Body quiz: Quiz): Call<Quiz>
+
+    @DELETE("/quizzes/{quizid}")
+    fun deleteQuiz(@Path("quizid") quizID: String): Call<Quiz>
 }
